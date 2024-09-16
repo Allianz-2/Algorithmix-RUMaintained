ee2396e4135fcc17c83f52007a06765f47775c90
Alianz-2 <spies.daniel20@gmail.com>
2024-09-16 18:45:28 +0200

    commit

--- 8-PHPTests/1-RetrieveUsers.php
+++ 8-PHPTests/1-RetrieveUsers.php
@@ -4,14 +4,23 @@ echo '<style>' . $cssFile . '</style>';
 ?>
 <?php
 // Connect to the database
-$servername = "localhost";
-$username = "your_username";
-$password = "your_password";
-$dbname = "your_database_name";
+$servername = "algorithmix-mysql.mysql.database.azure.com";
+$username = "AlgorithmixDaniel";
+$password = "Borderlands2";
+$dbname = "algorithmix";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+//Initializes MySQLi
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn,NULL,NULL, "..\CACertificate\DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+// Establish the connection
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
 // Retrieve all users from the database
