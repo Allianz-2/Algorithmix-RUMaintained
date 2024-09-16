9b6d90d7412d11a12557d6e8260526092ea0bc7e
Allianz-2 <155461114+Allianz-2@users.noreply.github.com>
2024-09-16 19:21:41 +0200

    modified:   5-UserSignInandRegistration/7-StudentRegistration.html new file:   5-UserSignInandRegistration/9-UserAddedSuccessful.php

---
+++ 5-UserSignInandRegistration/9-UserAddedSuccessful.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            background-color: purple;
+        }
+
+        .notification {
+            width: 300px;
+            height: 50px;
+            background-color: white;
+            color: black;
+            text-align: center;
+            margin: 20px auto;
+            padding: 10px;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+<body>
+    <div class="notification">
+        <!-- Your PHP code to display the notification message goes here -->
+    </div>
+</body>
+</html>
