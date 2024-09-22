78b3527cc33c7bb9a3834339c6fc12dca59abb8e
Allianz-2 <155461114+Allianz-2@users.noreply.github.com>
2024-09-22 14:36:40 +0200

    Refactor redirect path in secure.php

--- 5-UserSignInandRegistration/14-secure.php
+++ 5-UserSignInandRegistration/14-secure.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if (!isset($_SESSION["access"])) {
-        header("Location: 6-SignInPage.php");
+        header("Location: ../5-UserSignInandRegistration/6-SignInPage.php");
         exit();
     }
 ?>
