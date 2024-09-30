0eba1fe98f853700ba0dc8b013712241111ff080
Thulebona1 <tselethulebona@gmail.com>
2024-09-30 23:23:41 +0200

    commit

--- 10-MSDashboard/2-MSAssignedTasks.php
+++ 10-MSDashboard/2-MSAssignedTasks.php
@@ -126,8 +126,8 @@ th {
             <li><a href="5-MSHelp.php"><i class="fas fa-info-circle"></i>Help and Support</a></li>
         </ul>
         <div class="sidebar-footer">
-            <p><a href="#"><i class="fas fa-cog"></i> Settings</a></p>
-            <p><a href="..\5-UserSignInandRegistration\15-Logout.php" onclick="return confirmLogout()"><i class="fas fa-sign-out-alt"></i> Log Out</a></p>
+        <p><a href="..\6-UserProfileManagementPage\4-ProfileMS.php"><i class="fas fa-cog"></i> Settings</a></p>
+        <p><a href="..\5-UserSignInandRegistration\15-Logout.php" onclick="return confirmLogout()"><i class="fas fa-sign-out-alt"></i> Log Out</a></p>
         </div>
     </nav>
 
