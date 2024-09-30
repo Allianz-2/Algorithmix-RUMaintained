09b335d30a1f1d76cd3958cc401b82cc2a89a2d4
Allianz-2 <155461114+Allianz-2@users.noreply.github.com>
2024-09-30 18:19:01 +0200

    Update CSS file paths in registration pages

--- 5-UserSignInandRegistration/2-HallSecretaryRegistration.php
+++ 5-UserSignInandRegistration/2-HallSecretaryRegistration.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hall Secretary Registration - RUMaintained</title>
-    <link rel="stylesheet" href="11-Registration.css">
+    <link rel="stylesheet" href="../5-UserSignInandRegistration\5-CSS\11-Registration.css">
     <script src="12-RegistrationJavacript.js" defer></script>
 </head>
 <body>
