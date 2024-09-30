17a067799f6c78c5992e986f2f7aeddb8d773820
NalediMabusela <naledimabusela19@gmail.com>
2024-09-30 21:07:06 +0200

    Update 5-MSHelp.php

--- 10-MSDashboard/5-MSHelp.php
+++ 10-MSDashboard/5-MSHelp.php
@@ -243,8 +243,8 @@
 <body>
     <nav id="sidebar" class="sidebar">
         <div class="logo">
-        <span class="user-welcome">Welcome, User </span>
-                    <a href="user page"><i class="fas fa-user"></i></a> 
+        <span class="user-welcome">Welcome, <?php echo $_SESSION['Firstname']; ?></span> <!--  I THINK -->
+        <a href="..\6-UserProfileManagementPage\4-ProfileMS.php"><i class="fas fa-user"></i></a>
         </div>
         <ul>
             <li><a href="../1-GeneralPages\1-Home.php"><i class="fas fa-home"></i>Home</a></li>
